b13546084e0286b026d27d0580e290e1238aa0cd
Abhishek kumar <abhishekpatel66029@gmail.com>
2024-02-26 11:40:14 +0530

    added frontend url

--- frontend/src/const.js
+++ frontend/src/const.js
@@ -1,3 +1,3 @@
 export const server = "https://multivendor-ecommerce-api.onrender.com/api/v1";
-export const client = "http://localhost:5173";
+export const client = "https://shopshell.netlify.app";
 export const socket = "http://localhost:4000";
