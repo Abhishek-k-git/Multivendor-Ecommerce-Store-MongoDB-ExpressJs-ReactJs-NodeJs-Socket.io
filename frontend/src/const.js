e0797227701f4a3d1ba3acc0f8baca9a4f9855d1
Abhishek kumar <abhishekpatel66029@gmail.com>
2024-02-26 11:49:59 +0530

    added socket url

--- frontend/src/const.js
+++ frontend/src/const.js
@@ -1,3 +1,3 @@
 export const server = "https://multivendor-ecommerce-api.onrender.com/api/v1";
 export const client = "https://shopshell.netlify.app";
-export const socket = "http://localhost:4000";
+export const socket = "https://multivendor-ecommerce-socket.onrender.com";
