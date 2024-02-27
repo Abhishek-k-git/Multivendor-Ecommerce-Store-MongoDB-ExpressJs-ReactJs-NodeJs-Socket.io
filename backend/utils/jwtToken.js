54936aaf596024941ab405f643991331eae614a1
Abhishek kumar <abhishekpatel66029@gmail.com>
2024-02-27 09:19:38 +0530

    ...

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -2,8 +2,9 @@ const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
   const options = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
+    sameSite: "Lax",
     secure: true,
+    // path: "/",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
