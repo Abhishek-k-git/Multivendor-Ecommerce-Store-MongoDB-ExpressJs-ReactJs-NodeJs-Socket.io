79f98234bf65ac5fe8afaeee2754a28a9a02be3c
Abhishek kumar <abhishekpatel66029@gmail.com>
2024-03-03 19:03:16 +0530

    ...

--- frontend/src/const.js
+++ frontend/src/const.js
@@ -1,3 +1,3 @@
-export const server = "https://multivendor-ecommerce-api.onrender.com/api/v1";
+export const server = "/api/v1";
 export const client = "https://shopshell.netlify.app";
 export const socket = "https://multivendor-ecommerce-socket.onrender.com";
