6d8528b07a49faa4154d9ab53fff8097397c2c92
Abhishek kumar <abhishekpatel66029@gmail.com>
2024-03-04 21:22:35 +0530

    ...

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,6 +1,7 @@
 const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
   const options = {
+    domain: "shopshell.netlify.app",
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     sameSite: "None",
